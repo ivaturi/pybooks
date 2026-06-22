57cfd2fc2339019bdd449fd4ae17dcb13c71b7d0
abhi <abhi@mailbox.org>
2026-06-22 23:51:09 +0200

    use validate to tst load_books

--- test_crud.py
+++ test_crud.py
@@ -1,7 +1,6 @@
 from test_data import valid_books
 from crud import load_books, write_books
+from validate import validate
 
-books = load_books()
-print(books)
-
+validate(load_books())
 write_books(valid_books)
