9260d5302e24d57b2e660842f9f8ce17689167fc
abhi <abhi@mailbox.org>
2026-06-28 01:14:52 +0200

    type checking input

--- books.py
+++ books.py
@@ -6,13 +6,51 @@ def add_book():
     entries = schema.keys()
     book = {}
     for entry in entries:
-        question = f"Enter {entry}"
-        question += "*" if schema[entry].get("required") else ""
-        question += " (" + " | ".join(schema[entry].get("allowed_patterns")) + ")" if schema[entry].get("allowed_patterns") else ""
-        question += " (" + " | ".join(schema[entry].get("allowed_values")) + ")" if schema[entry].get("allowed_values") else ""
+    
+        # build the input question
+        question = f"Enter {entry}, expected format is '{schema[entry]['type'].__name__}' "
+        
+        # is the entry a required field?
+        if schema[entry].get("required"):
+            question += "(required)"
+
+        # does the entry have allowed patterns in the schema?
+        if schema[entry].get("allowed_patterns"):
+            question += f" ({' / '.join(schema[entry].get('allowed_patterns'))})"
+
+        # does the entry have allowed values in the schema?
+        if schema[entry].get("allowed_values"):
+            question += f" ({' / '.join(schema[entry].get('allowed_values'))})"
+
+        # does the entry have a minimum value in the schema?
+        if schema[entry].get("min"):
+            question += f" (min: {schema[entry].get('min')})"
+        
+        # does the entry have a maximum value in the schema?
+        if schema[entry].get("max"):
+            question += f" (max: {schema[entry].get('max')})"
+        
+        # does the entry expect a list from the user?
+        if schema[entry].get("type") == list:
+            question += f" (comma-separated list)"
+
+        # get value from user
         value = input(question + "\n: ")
-        if value != "":
-            book[entry] = value
+
+
+        if schema[entry]["type"] == list:
+            value = [x.strip() for x in value.split(",") if x.strip() != ""]
+            if len(value) > 0:
+                book[entry] = value
+        
+        elif schema[entry]["type"] == int and value != "":
+            book[entry] = int(value)
+        
+        else:
+            if len(value) > 0:
+                book[entry] = value
+    
+    
     crud.create(book)
 
 if len(argv) < 2:
