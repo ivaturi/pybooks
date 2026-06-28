8ac346cddd45fdb9fd691c12f7f74269ffbe863b
abhi <abhi@mailbox.org>
2026-06-28 19:10:23 +0200

    Splot load_books test into scenarios

--- test_crud.py
+++ test_crud.py
@@ -8,40 +8,39 @@ class TestCrud(unittest.TestCase):
 
     def setUp(self):
         
-        # test scenarios
-        self.test_scenarios = ["valid_books", "invalid_books"]
-        self.test_db_prefix = "books_test_db"
-
-        # create test dbs
-        for scenario in self.test_scenarios:
-            db_name = f"{self.test_db_prefix}_{scenario}.json"
-            utils.write_json(db_name, getattr(test_data, scenario))
-
+        self.test_db = "books_test_db"
         # corrupt json
-        db_name = f"{self.test_db_prefix}_corrupt_json.json"
-        with open(db_name,"w") as db:
+        with open(self.test_db,"w") as db:
             db.write(test_data.corrupt_json)
     
     def tearDown(self):
-        for scenario in self.test_scenarios:
-            db_name = f"{self.test_db_prefix}_{scenario}.json"
-            if os.path.exists(db_name):
-                os.remove(db_name)
+        if os.path.exists(self.test_db):
+            os.remove(self.test_db)
         
-        if os.path.exists(f"{self.test_db_prefix}_corrupt_json.json"):
-            os.remove(f"{self.test_db_prefix}_corrupt_json.json")
     
-    def test_load_books(self):
+    def test_load_valid_books(self):
         
         # happy path
-        scenario = self.test_scenarios[0]
-        self.assertEqual(getattr(test_data, scenario), crud.load_books(f"{self.test_db_prefix}_{scenario}.json"))
-
+        utils.write_json(self.test_db, test_data.valid_books)
+        self.assertEqual(test_data.valid_books, crud.load_books(self.test_db))
+   
+    def test_load_from_nonexistent_file(self):
         # load books fails if file doesn't exist
         self.assertEqual(False, crud.load_books("file_does_not_exist"))
 
-        # check if load_books fails to read corrupt json
-        self.assertEqual(False, crud.load_books(f"{self.test_db_prefix}_corrupt_json.json"))
+    def test_load_corrupt_json(self):
+        # corrupt json
+        with open(self.test_db,"w") as db:
+            db.write(test_data.corrupt_json)
+        self.assertEqual(False, crud.load_books(self.test_db))
+
+
+    def test_create_valid_book(self):
+
+        # grab a valid book entry
+        book = test_data.valid_books[0]
+
+
 
 if __name__ == "__main__":
     unittest.main()
